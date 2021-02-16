b45b459cd292b1bf0a1dff63b98d61a51fa5336b
Nathaniel Tjandra <ntjandra@ucsc.edu>
2021-02-16 11:48:58 -0800

    Add point redeemption logic

--- web_server/database.py
+++ web_server/database.py
@@ -15,16 +15,64 @@ def init_db():
 
 def show_db():
     c = conn.cursor()
-    c.execute('''SELECT * FROM transactions''')
-    print(c.fetchone())
+    c.execute('''SELECT * FROM transactions ORDER BY timestamp DESC''')
+    print("Fetch")
+    print(c.fetchall())
 
-# Initial Assumption: Timestamp is pre-formated correctly 
+# Initial Assumption: Timestamp is pre-formated correctly
 def insert_transaction(payer, points, timestamp):
     c = conn.cursor()
     record = (payer, points, timestamp)
     print("Inserting", record)
     c.execute('''INSERT INTO transactions VALUES (?,?,?)''', record)
 
+# Conditions: Point Value must never dip below zero
+# Requirement: Deletion happens from oldest transaction
+def spend_points(points):
+    c = conn.cursor()
+
+    # Keep track of individual's spending
+    clients = {}
+    usage = {}
+
+    # Get first 
+    c.execute('''SELECT payer, points FROM transactions ORDER BY timestamp ASC''')
+    result = c.fetchone()
+    while result != None:
+        payer, owned_points = result
+
+        # Check if the payer is new.
+        if payer not in clients:
+            clients[payer] = points
+            usage[payer] = 0
+
+        # subtract result with points until the value is zero.
+        if owned_points > clients[payer]:
+            spent_points = owned_points - clients[payer]
+            clients[payer] -= spent_points
+            usage[payer] += spent_points
+        else:
+            # Don't have enough points
+            # Deduct remainer into spent
+            usage[payer] += clients[payer]
+            clients[payer] = 0
+
+        # Grab next transaction
+        result = c.fetchone()
+    
+    # at the end return the amount spent
+    return clients
+
+### Test 1: Insertion
 init_db()
 insert_transaction("Nathan", 5000, "2020-11-02T14:00:00Z")
+insert_transaction("DANNON", 1000, "2020-11-02T14:00:00Z")
+insert_transaction("UNILEVER", 200,"2020-10-31T11:00:00Z")
+insert_transaction("DANNON", -200, "2020-10-31T15:00:00Z")
+insert_transaction("MILLER COORS", 10000, "2020-11-01T14:00:00Z")
+insert_transaction("DANNON", 300, "2020-10-31T10:00:00Z")
 show_db()
+
+### Test 2: Update, based on Redemption
+spend_points(5000)
+show_db
