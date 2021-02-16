9f92bc54a7dd45015e5ca05bcb7ae5a9a5849082
Nathaniel Tjandra <ntjandra@ucsc.edu>
2021-02-16 10:12:08 -0800

    Added Redeem/Spend Route

--- web_server/routes.py
+++ web_server/routes.py
@@ -10,7 +10,7 @@ from flask import jsonify, flash, redirect, request, abort, Response
 from web_server import app
 
 # Adds a transaction given Payer's name, points, and timestamp 
-@app.route("/api/user/add", methods=['PUT'])
+@app.route("/api/user/transaction", methods=['PUT'])
 def add_transaction():
     form = request.json
     payer = str(form['payer'])
@@ -19,3 +19,12 @@ def add_transaction():
     # TODO: Replace with the DB call/action
     # print(payer, points, timestamp)
     return Response('{"message":"Transaction Added"}', status=202)
+
+# Redeem points for a reward
+@app.route("/api/user/redeem", methods=['PUT'])
+def redeem_points():
+    form = request.json
+    points = int(form['points'])
+    return Response('{"message":"Spent ' + str(points) + ' points"}')
+    # TODO: Call to Database to deduct the points from the balance
+    # Return a list of jsons of each user's deducted value
